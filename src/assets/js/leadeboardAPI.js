da7e47b2d2d60bdea94e3ff354c5fa98d49d041d
Lameck1 <lameck72@gmail.com>
2021-07-28 18:29:45 +0300

    Add `postScores` and `getScores` methods
    - `postScores` updates the API with new scores
    - `getScores` makes a get request to the API to get all scores and then display them on the UI

---
+++ src/assets/js/leadeboardAPI.js
@@ -0,0 +1,38 @@
+import 'regenerator-runtime/runtime.js';
+import getElement from './helpers/getElement.js';
+import showScores from './helpers/showScores.js';
+
+const API_KEY = 'hGgv9MCCAVToMVm7R67v';
+const URI = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${API_KEY}/scores`;
+
+export const postScores = async (user, score) => {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const newScore = JSON.stringify({ user, score });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: newScore,
+    redirect: 'follow',
+  };
+
+  await fetch(URI, requestOptions)
+    .then((response) => response.text())
+    .then((result) => result)
+    .catch((error) => error);
+};
+
+export const getScores = async () => {
+  const scoreList = getElement('.score-list');
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+
+  await fetch(URI, requestOptions)
+    .then((response) => response.json())
+    .then((result) => showScores(result, scoreList))
+    .catch((error) => error);
+};
