05e81a0b327b593c498b56ea81a59aa9f98100f9
Lameck1 <lameck72@gmail.com>
2021-08-06 09:52:57 +0300

    Refactor #showScores
    Refactores this method to use Object destructuring

--- src/assets/js/helpers/showScores.js
+++ src/assets/js/helpers/showScores.js
@@ -1,16 +1,14 @@
 import createElement from './createElement.js';
 
 export default (data, scoreList) => {
+  console.log(data);
   const { result } = data;
-  const scores = [];
-  for (let i = 0; i < result.length; i += 1) {
-    scores.push([result[i].user, result[i].score]);
-  }
+  const scores = result.map(({ user, score }) => [user, score]);
   scores.sort((a, b) => b[1] - a[1]);
   scoreList.textContent = '';
 
   scores.forEach((score) => {
-    const listItem = createElement('li', {}, `${score[0]}: ${score[1]}`);
+    const listItem = createElement('li', {}, `${score[0]} : ${score[1]}`);
     scoreList.append(listItem);
   });
 
