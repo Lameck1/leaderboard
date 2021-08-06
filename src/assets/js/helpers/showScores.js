ceb21aa6cd5438b969ca37cb58ec0cc1e3158260
Lameck1 <lameck72@gmail.com>
2021-08-06 09:55:08 +0300

    Remove debugging instructions

--- src/assets/js/helpers/showScores.js
+++ src/assets/js/helpers/showScores.js
@@ -1,7 +1,6 @@
 import createElement from './createElement.js';
 
 export default (data, scoreList) => {
-  console.log(data);
   const { result } = data;
   const scores = result.map(({ user, score }) => [user, score]);
   scores.sort((a, b) => b[1] - a[1]);
